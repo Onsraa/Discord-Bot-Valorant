1d69c4053ada61b74f1e7280298d6df840b65949
Teddy TRUONG <truongteddy306@icloud.com>
2023-04-14 14:00:29 +0200

    line code added to prevent duplicates in prompt chat during commands call

--- events/ready.js
+++ events/ready.js
@@ -5,5 +5,8 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		
+		//Clears commands repetitions
+		//client.application.commands.set([])
 	},
 };
