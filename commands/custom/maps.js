4b911be23a1b64dcf950f4e3f58e26b9c55bf15d
Teddy TRUONG <truongteddy306@icloud.com>
2023-04-17 20:25:35 +0200

    options for command tested, ideas must be developed later

--- commands/custom/maps.js
+++ commands/custom/maps.js
@@ -2,36 +2,35 @@ const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, Embed
 const { maps } = require('./maps.json');
 
 module.exports = {
-    cooldown: 5,
 	data: new SlashCommandBuilder()
 		.setName('map')
 		.setDescription('Generate a random map.'),
+        /*
+        //That part could add options to the command prompt so the bot could choose without a doubt the right custom party the generated map.
+        .addStringOption(option =>
+            option.setName('category')
+                .setDescription('The gif category')
+                .setRequired(true)
+                .addChoices(
+                    { name: generateRandomString(6), value: 'none' },
+                    { name: generateRandomString(6), value: 'none' },
+                    { name: generateRandomString(6), value: 'none' },
+                )),
+        */
 	async execute(interaction) {
         if (!interaction.isChatInputCommand()) return;
 
-        let tmp_maps = maps;
-
+        //Generate buttons and then add them to the row.
         const rerollButton = createRerollButton();
         const validateButton = validateMapButton();
         const row = new ActionRowBuilder()
             .addComponents(validateButton, rerollButton)
 
+        //Generate a random map.
         const mapEmbed = createMapEmbed();
-
+        
 		const response = await interaction.reply({embeds: [mapEmbed], components: [row]});
-
-        const filter = i => i.user.id === interaction.user.id;
-        try {
-            const confirmation = await response.awaitMessageComponent({ filter, time: 10000 });
-            if(confirmation.customId === 'validate'){
-                await i.update({content : 'It\'s validate !', components: []});
-            }else if (confirmation.customId === 'reroll'){
-                await i.update({content : 'Reroll !', components: []});
-            }
-        } catch (e) {
-            await response.editReply({ content: 'Confirmation not received within 10 seconds, cancelling', components: [] });
-        }
-	},
+	}
 };
 
 function createMapEmbed(){
@@ -60,45 +59,15 @@ function validateMapButton(){
 }
 
 /*
-
- async execute(interaction) {
-        const map = maps[Math.floor(Math.random() * maps.length)];
-        const embed = new MessageEmbed()
-            .setTitle(map.name)
-            .setImage(map.image);
-        const row = new MessageActionRow().addComponents(
-            createRerollButton()
-        );
-
-        const message = await interaction.reply({
-            content: 'Here is your random Valorant map:',
-            embeds: [embed],
-            components: [row],
-        });
-
-        const collector = interaction.channel.createMessageComponentCollector({
-            filter: (i) => i.customId === 'reroll' && i.user.id === interaction.user.id,
-            time: 10000,
-            max: 1,
-        });
-
-        collector.on('collect', async i => {
-            if (i instanceof ButtonInteraction) {
-                await i.deferUpdate();
-
-                const newMap = maps[Math.floor(Math.random() * maps.length)];
-                const newEmbed = new MessageEmbed()
-                    .setTitle(newMap.name)
-                    .setImage(newMap.image);
-
-                row.components[0] = createRerollButton();
-
-                await i.update({
-                    content: 'Here is your new random Valorant map:',
-                    embeds: [newEmbed],
-                    components: [row],
-                });
-            }
-        });
-    },
-    */
+//Will be used later to generate custom's name
+function generateRandomString(length) {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+  
+    for (let i = 0; i < length; i++) {
+      result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+  
+    return result;
+  }
+*/
